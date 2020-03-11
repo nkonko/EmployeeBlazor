bca13328cd2a186c63d8147a4616789a6c48a6c1
nicolas.azzara <nicolas.azzara@intive-fdv.com>
2020-03-11 14:55:50 -0300

    fixed add employee

---
+++ BlazorApp4/Client/Pages/Employees/AddEmployee.razor.cs
@@ -0,0 +1,29 @@
+﻿namespace BlazorApp4.Client.Pages.Employees
+{
+    using BlazorApp4.Shared.Models;
+    using Microsoft.AspNetCore.Components;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    public partial class AddEmployee
+    {
+        private Employee employee = new Employee();
+
+        private bool isSuccess = false;
+        private string message = "";
+
+        public async Task PostEmployee()
+        {
+            await httpClient.SendJsonAsync(HttpMethod.Post, $"api/Employees", employee);
+            isSuccess = true;
+            message = "Done!";
+            ClearFields();
+            StateHasChanged();
+        }
+
+        private void ClearFields()
+        {
+            employee.Name = string.Empty;
+            employee.Area = string.Empty;
+        }
+    }
+}
