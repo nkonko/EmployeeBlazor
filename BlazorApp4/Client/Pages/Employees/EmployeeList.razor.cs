616f80b1ff84eea0620c2d3ed413cf4087aa0782
nicolas.azzara <nicolas.azzara@intive-fdv.com>
2020-03-17 14:10:26 -0300

    finished CRUD

---
+++ BlazorApp4/Client/Pages/Employees/EmployeeList.razor.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorApp4.Client.Pages.Employees
+{
+    using BlazorApp4.Shared.Models;
+    using Microsoft.AspNetCore.Components;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public partial class EmployeeList
+    {
+        bool showModal = false;
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        private Employee employee = new Employee();
+
+        protected override async Task OnInitializedAsync() => await GetEmployees();
+
+        void ModalShow() => showModal = true;
+        void ModalCancel() => showModal = false;
+
+        public async Task DeleteEmployee(int Id)
+        {
+            await GetEmployee(Id);
+            await httpClient.SendJsonAsync(HttpMethod.Delete, $"api/Employees/{Id}", employee);
+            showModal = false;
+            await GetEmployees();
+            StateHasChanged();
+        }
+
+        private async Task GetEmployee(int Id)
+        {
+            employee = await httpClient.GetJsonAsync<Employee>($"api/Employees/{Id}");
+        }
+
+        private async Task GetEmployees()
+        {
+            Employees = await httpClient.GetJsonAsync<List<Employee>>("api/Employees");
+        }
+    }
+}
---
+++ BlazorApp4/Client/Pages/Employees/UpdateEmployee.razor.cs
@@ -0,0 +1,38 @@
+﻿namespace BlazorApp4.Client.Pages.Employees
+{
+    using BlazorApp4.Shared.Models;
+    using Microsoft.AspNetCore.Components;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public partial class UpdateEmployee
+    {
+        [Parameter]
+        public long Id { get; set; }
+
+        private Employee employee = new Employee();
+
+        private bool isSuccess = false;
+        private string message = "";
+
+        protected override async Task OnInitializedAsync()
+        {
+            employee = await httpClient.GetJsonAsync<Employee>($"api/Employees/{Id}");
+        }
+
+        public async Task EditEmployee()
+        {
+            await httpClient.SendJsonAsync(HttpMethod.Put, $"api/Employees/{Id}", employee);
+            isSuccess = true;
+            message = "Done!";
+            ClearFields();
+            StateHasChanged();
+        }
+
+        private void ClearFields()
+        {
+            employee.Name = string.Empty;
+            employee.Area = string.Empty;
+        }
+    }
+}
